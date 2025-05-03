e393666c9ed7f4bbf2f8854246a1f8dbfc66d1ff
Bruno <brunobuenopovoa01@gmail.com>
2025-05-03 16:08:31 -0300

    Logins e first admin

--- src/db/database.js
+++ src/db/database.js
@@ -1,18 +1,32 @@
 const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-const dbFile = path.join(__dirname, 'database.sqlite');
-const schemaPath = path.join(__dirname, 'schema.sql');
-const db = new sqlite3.Database(dbFile);
+// Caminho do banco de dados
+const dbPath = path.join(__dirname, 'database.sqlite');
 
-const schema = fs.readFileSync(schemaPath, 'utf8');
-db.exec(schema, (err) => {
-  if (err) {
-    console.error('Erro ao criar estrutura do banco:', err.message);
-  } else {
-    console.log('Banco de dados inicializado com sucesso.');
-  }
+// Verificar se o banco de dados já existe
+const dbExists = fs.existsSync(dbPath);
+
+// Conectar ou criar o banco de dados
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados', err);
+    } else {
+        console.log('Conectado ao banco de dados');
+    }
 });
+
+// Criar as tabelas se o banco não existir
+if (!dbExists) {
+    const schema = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+    db.exec(schema, (err) => {
+        if (err) {
+            console.error('Erro ao criar tabelas:', err);
+        } else {
+            console.log('Tabelas criadas com sucesso!');
+        }
+    });
+}
 
 module.exports = db;
